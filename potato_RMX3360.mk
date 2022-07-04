7c579b4ca9be199e3237c998bc8c75435df9ce3e
clawside <enyerplayus@gmail.com>
2022-07-04 20:49:06 +0000

    RMX3360: POSP BringUp
    Signed-off-by: clawside <enyerplayus@gmail.com>

--- potato_RMX3360.mk
+++ potato_RMX3360.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from RMX3360 device
 $(call inherit-product, device/oplus/RMX3360/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common POSP stuff.
+$(call inherit-product, vendor/potato/config/common.mk)
 
-PRODUCT_NAME := lineage_RMX3360
+PRODUCT_NAME := potato_RMX3360
 PRODUCT_DEVICE := RMX3360
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
